63e2388c0bab572bd72350b4ea2bdf2f0a43746d
Avraham Mahfuda <avraham.mahfuda@bloxroute.com>
2019-04-21 13:25:52 +0000

    added an installation rpath logic

--- run_tests.py
+++ run_tests.py
@@ -5,8 +5,8 @@ import subprocess
 
 def main():
     root_dir = os.path.dirname(os.path.abspath(__file__))
-    bin_dir = os.path.join(root_dir, "bin")
-    test_exec_files = glob.glob(os.path.join(bin_dir, "check_*"))
+    tests_dir = os.path.join(root_dir, "tests")
+    test_exec_files = glob.glob(os.path.join(tests_dir, "check_*"))
     for test_exec_file in test_exec_files:
         subprocess.check_call(test_exec_file)
 

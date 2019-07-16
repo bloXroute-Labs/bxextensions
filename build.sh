6e9165e434f1854d49c9c2fd00cf201674c7d9a7
urimargalit-bloxroute <43845201+Uri-Margalit-bloXroute@users.noreply.github.com>
2019-07-16 17:46:29 +0300

    running the docker commands in parallel (#35)
    * running the docker commands in parallel
    * script exit with error if one of the docker builds failed

--- build.sh
+++ build.sh
@@ -1,30 +1,53 @@
 #!/usr/bin/env bash
 
-set -e
+OS_LIST=${1:-alpine-3.8}
+PIDS=""
+STATUS=""
+OS_COUNT=1
+RETURN_CODE=0
 
-OS_RELEASE=${1:-alpine-3.8}
+for os in $OS_LIST
+do
+  if (( $(docker container ls -a | grep -c "bxextensions_${os} ") > 0 )); then
+      echo "removing bxextensions_${os} container"
+      docker container rm bxextensions_${os}
+  else
+      echo "building bxextensions_${os} container for the first time"
+  fi
+  mkdir -p release/${os}
+  mkdir -p build/${os}
+  rm -f release/${os}/*
+  ROOT_DIR=$(pwd)
+  rm -f ${ROOT_DIR}/*.so
+  rm -f ${ROOT_DIR}/*.dylib
 
-if (( $(docker container ls -a | grep -c "bxextensions_${OS_RELEASE} ") > 0 )); then
-    echo "removing bxextensions_${OS_RELEASE} container"
-    docker container rm bxextensions_${OS_RELEASE}
-else
-    echo "building bxextensions_${OS_RELEASE} container for the first time"
-fi
-mkdir -p release/${OS_RELEASE}
-mkdir -p build/${OS_RELEASE}
-rm -f release/${OS_RELEASE}/*
-ROOT_DIR=$(pwd)
-rm -f ${ROOT_DIR}/*.so
-rm -f ${ROOT_DIR}/*.dylib
+  DOCKER_FILE="Dockerfile-$os"
 
-DOCKER_FILE="Dockerfile-$OS_RELEASE"
+  echo "Building container from $DOCKER_FILE"
+  bash -c "docker build -f ${DOCKER_FILE} -t bxextensions_${os} --build-arg UID=$(id -u) --build-arg GID=$(id -g) . ;\
+           docker run --name bxextensions_${os} \
+               	--volume ${ROOT_DIR}/release/${os}:/app/bxextensions/release \
+         	--volume ${ROOT_DIR}/build/${os}:/app/bxextensions/build \
+        	--user $(id -u):$(id -g) \
+  	        bxextensions_${os}" &
+  PIDS[$OS_COUNT]=$!
+  echo "docker run of $os process id; ${PIDS[$OS_COUNT]}"
+  ((OS_COUNT+=1))
+done
+((OS_COUNT-=1))
 
-echo "Building container from $DOCKER_FILE"
-docker build -f ${DOCKER_FILE} -t bxextensions_${OS_RELEASE} --build-arg UID=$(id -u) --build-arg GID=$(id -g) .
+for os_seq in $(seq 1 $OS_COUNT)
+do
+  wait ${PIDS[$os_seq]}
+  STATUS[$os_seq]="$?"
+done
 
-echo "running docker container"
-docker run --name bxextensions_${OS_RELEASE} \
-	--volume ${ROOT_DIR}/release/${OS_RELEASE}:/app/bxextensions/release \
-	--volume ${ROOT_DIR}/build/${OS_RELEASE}:/app/bxextensions/build \
-	--user $(id -u):$(id -g) \
-	bxextensions_${OS_RELEASE}
+for os_seq in $(seq 1 $OS_COUNT)
+do
+  if (( ${STATUS[$os_seq]} > 0 )); then
+    echo "process ${PIDS[$os_seq]} failed with exit code: ${STATUS[$os_seq]}"
+    RETURN_CODE=${STATUS[$os_seq]}
+  fi
+done
+
+exit $RETURN_CODE
